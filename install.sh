a644e6b40092d11cd6e450a3ae352b5bb8d239f9
JavierC <66850752+javiercplus@users.noreply.github.com>
2026-09-05 13:49:10 -0400

    Unblock Bluetooth and start Bluetooth service

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 xbps-install -Sy bluez blueman bluetui libspa-bluetooth
 ln -s /etc/sv/bluetoothd /var/service/
+rfkill unblock bluetooth
 sv up bluetoothd
 sv start bluetoothd
